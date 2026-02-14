9614b25717eb4cb1e85b8c598df6452f64d2e1b2
ilia <iliagholami151@gmail.com>
2026-02-13 19:15:18 -0800

    git pwa 33

--- app/RootClientLayout.tsx
+++ app/RootClientLayout.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { usePathname } from "next/navigation";
-import { useEffect } from "react";
 import MusicPlayer from "@/components/common/MusicPlayer/MusicPlayer";
 import BottomNav from "@/components/common/BottomNav/BottomNav";
 import { useAppSelector } from "@/redux/hook";
@@ -11,9 +10,7 @@ export default function RootClientLayout({ children }: { children: React.ReactNo
   const hidePlayerAndNav = pathname === "/auth/login";
   const hasSong = useAppSelector((state) => state.player.queue.length > 0);
 
-  useEffect(() => {
-    document.body.style.paddingBottom = 'env(safe-area-inset-bottom)';
-  }, []);
+  
 
   return (
     <>
