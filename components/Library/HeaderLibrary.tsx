db58bdda0702e1f85f7fd819d4d6da395dd8e2ba
ilia <iliagholami151@gmail.com>
2026-02-15 13:37:42 -0800

    git pwa 33

--- components/Library/HeaderLibrary.tsx
+++ components/Library/HeaderLibrary.tsx
@@ -4,9 +4,9 @@ import PlusIconCs from "@/public/Icons/PlusIconCs"
 const HeaderLibrary = () => {
   return (
     <div className="w-full h-9 flex items-center justify-between">
-      <div className="relative flex w-37.75 items-center ">
+      <div className="relative flex  items-center ">
          <OverlayLeft />
-         <p className="text-[24px] text-[#FFFFFF] mt-0.5 font-bold left-11.25 relative max-[340px]:text-[20px] max-[340px]:top-0.5 max-[390px]:text-[21px]">Your Library</p>
+         <p className="text-[23px] text-[#FFFFFF] mt-0.5 font-bold left-11.25 relative max-[340px]:text-[20px] max-[340px]:top-0.5 ">Your Library</p>
       </div>
       <div className="w-15.75 h-5 flex gap-5.75 items-center">
         <SearchIconCs fill="white" fillOpacity="0.8"/>
